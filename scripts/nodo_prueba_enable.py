c744cf0a07440032d149688d27d6544cd33aa33d
jdserranomu <jd.serranomu@uniandes.edu.co>
2019-07-19 11:47:35 -0500

    Se creo nodo de prueba para servicio de enable

---
+++ scripts/nodo_prueba_enable.py
@@ -0,0 +1,20 @@
+from master_msgs.srv import service_enable
+import rospy
+
+def nodo_prueba_enable():
+    rospy.init_node('nodo_prueba_enable', anonymous=True)
+    s = rospy.Service('service_enable', service_enable, handle_prueba)
+    rospy.spin()
+    pass
+
+
+def handle_prueba(param):
+    print("Llego:", param.message)
+    return []
+
+
+if __name__ == '__main__':
+    try:
+        nodo_prueba_enable()
+    except rospy.ROSInterruptException:
+        pass
